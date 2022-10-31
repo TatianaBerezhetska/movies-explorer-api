6df22b24f4fa83e48d17e67b93c012f89eaaac2b
Tatiana Berezhetskaya <t.berezhetskaya@gmail.com>
2022-10-31 16:08:32 +1000

    fixed getMovies controller by user id

--- controllers/movies.js
+++ controllers/movies.js
@@ -5,7 +5,7 @@ const ForbiddenError = require('../errors/forbidden-err');
 const NotFoundError = require('../errors/not-found-err');
 
 const getMovies = (req, res, next) => {
-  Movie.find({})
+  Movie.find({ owner: req.user._id })
     .then((movies) => res.send(movies))
     .catch((err) => {
       next(err);
